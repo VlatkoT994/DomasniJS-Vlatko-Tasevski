2c5b6c5d8085bd13a5a3cedb418c3b540530a3bc
Sanja Tasevska <vlatko.tasevskii@gmail.com>
2020-01-16 19:41:02 +0100

    'domasna'

---
+++ find zero/index.js
@@ -0,0 +1,12 @@
+let findZero = function(start)
+{
+    let result = []
+    if(start === 2)
+        result = result.concat(start)
+    else if (typeof(start)==='object')
+        for (let i = 0; i < start.length; i++)
+            result = result.concat(findZero(start[i]))
+    return result
+}
+let arr = [[[2, [1, [[4, [[0], 5]]]]]]];
+console.log(findZero(arr))
